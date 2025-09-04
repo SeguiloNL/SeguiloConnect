840c3951ed847792b6c3f3457a8b1f55511bebae
SeguiloNL <bas@seguilo.nl>
2025-09-04 14:48:08 +0200

    Update header.php

--- views/header.php
+++ views/header.php
@@ -127,12 +127,6 @@ try {
         </li>
       </ul>
 
-      <?php if (!empty($_SESSION['user']['role']) && $_SESSION['user']['role'] === 'Super-admin'): ?>
-<li class="nav-item">
-  <a class="nav-link" href="index.php?route=vendor_orders">Order activatie</a>
-</li>
-<?php endif; ?>
-
       <!-- Rechterzijde: Mijn account -->
       <ul class="navbar-nav ms-auto mb-2 mb-lg-0 align-items-center">
         <li class="nav-item dropdown">
